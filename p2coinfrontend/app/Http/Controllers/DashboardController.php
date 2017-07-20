bb7a3304a53694e4785751b41b3145a4470b666d
Webhero9297 <webhero9297@mail.com>
2017-07-20 18:14:25 -0500

    wasabi
    Bitcoin Escrow

--- p2coinfrontend/app/Http/Controllers/DashboardController.php
+++ p2coinfrontend/app/Http/Controllers/DashboardController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\WalletManage;
 class DashboardController extends Controller
 {
     //
     public function index() {
+        $model = new WalletManage();
+        //002$wallet_arr = $model->generateWallet();
+        //$sendAmount = bcmul($getBalanceInfo->data->available_balance, '0.01', 8);
+        //dd($model->getWalletBalanceByAddress('3PJiUQuKbY3bBZotgUzxXRZbdtndMCCACm'));
+        dd($model->sendCoin(0.001002,'2N2Sgn1wdwHQbsgEZuGJw882XKdJg5vJN8v', '2NEq4MgBwkrwk3borzpHSK9SEmLW93wRpUY',$model->getPinCode()));
         return view('index.dashboard');
     }
 }
