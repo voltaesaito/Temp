cd5f52ba995f4ccfd6c5cab95f7aac70de980fe8
Webhero9297 <webhero9297@mail.com>
2017-07-30 18:41:10 -0500

    wasabi

--- P2CoinAdmin/app/Http/Controllers/IndexController.php
+++ P2CoinAdmin/app/Http/Controllers/IndexController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index() {
-        return view('index.index');
+        $total_users = 500;
+        $active_users = 300;
+        $user_rate = $active_users/$total_users*100;
+
+        $nowdate = date("Y-m-d H:m:s", strtotime(date("Y-m-d H:m:s")));
+        $yesterday = date("Y-m-d H:m:s", strtotime("-1 days", $nowdate));
+        // dd(date("Y-m-d H:m:s", $nowdate), date("Y-m-d H:m:s", $yesterday));
+        return view('index.index')->with(['total_users'=>$total_users, 'active_users'=>$active_users,'user_rate'=>$user_rate]);
     }
 }
